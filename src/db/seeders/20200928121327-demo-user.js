11c55e1ec27f10dea14e299f249ed571586897e5
Pupye <fullhouseya89@gmail.com>
2020-09-30 15:53:55 +0600

    update user seed with notes

--- src/db/seeders/20200928121327-demo-user.js
+++ src/db/seeders/20200928121327-demo-user.js
@@ -2,13 +2,196 @@
 const bcrypt = require('bcrypt')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('users', [{
+    await queryInterface.bulkInsert('users', [{
       userName: 'test',
       password: bcrypt.hashSync('testtest', 1)
     }])
+
+    const userIds = await queryInterface.sequelize.query(
+      'SELECT id from users'
+    )
+
+    await queryInterface.bulkInsert('notes', [
+      {
+        title: 'test',
+        content: 'testtest',
+        authorId: userIds[0].id,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString()
+      },
+      {
+        title: 'test',
+        content: 'testtest',
+        authorId: userIds[0].id,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString()
+      },
+      {
+        title: 'test',
+        content: 'testtest',
+        authorId: userIds[0].id,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString()
+      },
+      {
+        title: 'test',
+        content: 'testtest',
+        authorId: userIds[0].id,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString()
+      },
+      {
+        title: 'test',
+        content: 'testtest',
+        authorId: userIds[0].id,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString()
+      },
+      {
+        title: 'test',
+        content: 'testtest',
+        authorId: userIds[0].id,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString()
+      },
+      {
+        title: 'test',
+        content: 'testtest',
+        authorId: userIds[0].id,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString()
+      },
+      {
+        title: 'test',
+        content: 'testtest',
+        authorId: userIds[0].id,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString()
+      },
+      {
+        title: 'test',
+        content: 'testtest',
+        authorId: userIds[0].id,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString()
+      },
+      {
+        title: 'test',
+        content: 'testtest',
+        authorId: userIds[0].id,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString()
+      },
+      {
+        title: 'test',
+        content: 'testtest',
+        authorId: userIds[0].id,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString()
+      },
+      {
+        title: 'test',
+        content: 'testtest',
+        authorId: userIds[0].id,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString()
+      },
+      {
+        title: 'test',
+        content: 'testtest',
+        authorId: userIds[0].id,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString()
+      },
+      {
+        title: 'test',
+        content: 'testtest',
+        authorId: userIds[0].id,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString()
+      },
+      {
+        title: 'test',
+        content: 'testtest',
+        authorId: userIds[0].id,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString()
+      },
+      {
+        title: 'test',
+        content: 'testtest',
+        authorId: userIds[0].id,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString()
+      },
+      {
+        title: 'test',
+        content: 'testtest',
+        authorId: userIds[0].id,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString()
+      },
+      {
+        title: 'test',
+        content: 'testtest',
+        authorId: userIds[0].id,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString()
+      },
+      {
+        title: 'test',
+        content: 'testtest',
+        authorId: userIds[0].id,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString()
+      },
+      {
+        title: 'test',
+        content: 'testtest',
+        authorId: userIds[0].id,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString()
+      },
+      {
+        title: 'test',
+        content: 'testtest',
+        authorId: userIds[0].id,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString()
+      },
+      {
+        title: 'test',
+        content: 'testtest',
+        authorId: userIds[0].id,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString()
+      },
+      {
+        title: 'test',
+        content: 'testtest',
+        authorId: userIds[0].id,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString()
+      },
+      {
+        title: 'test',
+        content: 'testtest',
+        authorId: userIds[0].id,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString()
+      },
+      {
+        title: 'test',
+        content: 'testtest',
+        authorId: userIds[0].id,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString()
+      }
+    ])
   },
 
   down: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('Users', null, {})
+    await queryInterface.bulkDelete('notes', null, {})
+    await queryInterface.bulkDelete('users', null, {})
   }
 }
