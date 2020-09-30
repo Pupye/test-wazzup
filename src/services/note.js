d4b8b0c486aa6629abe15422857215f2e0ff56b0
Pupye <fullhouseya89@gmail.com>
2020-09-30 14:26:07 +0600

    check existence of user

--- src/services/note.js
+++ src/services/note.js
@@ -1,8 +1,14 @@
+const UserError = require('../errors/userError')
+
 module.exports = (ctx) => {
   const { db } = ctx
 
   return {
     createNote: async (note, authorId) => {
+      const exists = await db.User.findByPk(authorId)
+      if (!exists) {
+        throw new UserError('user does not exists', 403)
+      }
       const userNote = {
         ...note,
         authorId
